d381c782e376fa36a1a6181994e6f44410cd0502
Smkyu <smcgrat3@oswego.edu>
2017-03-25 00:57:26 -0400

    Add files via upload

--- ParkingLot.java
+++ ParkingLot.java
@@ -1,57 +1,68 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.csc380homework;
 
-import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Locale;
 
 /**
  *
- * @author sean
+ * @author Sean McGrath
  */
 public class ParkingLot {
-
-    ArrayList<Car> lot = new ArrayList<>();
-
-    public boolean isEmpty() {
-        if (lot.isEmpty()) {
-            return true;
-        } else {
-            return false;
-        }
-    }
-
-    public void addCar(Car c) {
-        if (lot.size() < 200) {
-            lot.add(c);
-        } else {
-
-        }
+    
+    private final String parkingLotName;
+    
+    private final Parking_Spot lot[];
+    private final int max; //how many parking spots there are, might not need later on
+    
+    
+    
+    public void addReservation(){ //parameters to be added later        
+        //unwritten so far
+        // make a reservation
     }
     
-    public boolean isTimeAvailable(String time){
-    if(time.isTimeAvailable){
+    public String getLotName() {
+        return parkingLotName;
+    }
+    
+    public boolean spotsAvailbleAtTimeAndDay(int day, int time){
+        //check if a reservation can be made at such and such day/time    !unwritten!
         return true;
-    }else{
-        return false;
-    }
     }
     
-
-    public void removeCar(Car c) {
-        lot.remove(c);
-    }
-
-    public boolean isFull() {
-        if (lot.size() == 200) {
-            return true;
-        } else {
-            return false;
-        }
-    }
-
-    public boolean isTaken(int spot) {
-        if (spot.isTaken()){
-            return true;
-        } else {
-            return false;
-        }
+    private static boolean isLeapYear(int year) {
+        Calendar cal = Calendar.getInstance();
+        cal.set(Calendar.YEAR, year);
+        return cal.getActualMaximum(Calendar.DAY_OF_YEAR) > 365;
     }
     
+    public ParkingLot(int spots, String lotName){
+        
+        String month = Calendar.getInstance().getDisplayName(Calendar.MONTH, Calendar.LONG, Locale.getDefault());
+        int year = Calendar.getInstance().get(Calendar.YEAR);
+        int m;
+        if (isLeapYear(year) && Calendar.getInstance().get(Calendar.MONTH) == 1) {
+            m = 0;
+        } else {
+            m = Calendar.getInstance().get(Calendar.MONTH)+1;
+        }
+        
+        Parking_Spot p;
+                
+        lot = new Parking_Spot[spots];
+        for (int i = 0; i < spots; i++) {//initializes all the parking spots
+            p = new Parking_Spot(i+1, m, month);
+            lot[i] = p;
+        }
+        parkingLotName = lotName;
+        max = spots;
+    }
+    
+    //maybe more methods later as needed
+    //isEmpty nd isFull are useless in this iteration(reservations based on an interface instead of keeping track of the parking lot with some sort of scanner), their usablity has been taken over by spotsAvailbleAtTimeAndDay
 }
